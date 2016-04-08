70844ba71544a6c249dd692bc81f1540f49daa65
xaos <vladimir.p.polyakov@gmail.com>
2016-04-08 15:55:48 +0400

    lldp finished

--- main.cpp
+++ main.cpp
@@ -26,9 +26,18 @@ void iterate_devs(std::string hostname, std::string osname) {
 
     PIP_ADAPTER_INFO pAdapterInfo = AdapterInfo;
     while (pAdapterInfo) {
+        IP_ADDRESS_STRING ipaddress = pAdapterInfo->IpAddressList.IpAddress;
+        if (&ipaddress == nullptr || strlen(ipaddress.String) < 7) {
+            continue;
+        }
+        int ip1, ip2, ip3, ip4;
+        char dot;
+        istringstream s(ipaddress.String);  // input stream that now contains the ip address string
+        s >> ip1 >> dot >> ip2 >> dot >> ip3 >> dot >> ip4 >> dot;
+
         pcap_t *fp;
         char errbuf[PCAP_ERRBUF_SIZE];
-        u_char packet[100];
+        u_char packet[1024];
         int i;
 
         string rpcap = "rpcap://\\Device\\NPF_";
@@ -89,10 +98,119 @@ void iterate_devs(std::string hostname, std::string osname) {
             packet[counter++] = 0x00; // 120 sec
             packet[counter++] = 120;
 
-            // TODO
+            // Port description
+            packet[counter++] = 0x08; // Port Description
+            int descLen = strlen(pAdapterInfo->Description);
+            packet[counter++] = (u_char) descLen; // Description length
+            for (int j = 0; j < descLen; ++j) {
+                packet[counter++] = (u_char) pAdapterInfo->Description[j];
+            }
+
+            // System name
+            packet[counter++] = 0x0a; // System name
+            packet[counter++] = (u_char) hostname.length(); // Name length
+            for (int j = 0; j < hostname.length(); ++j) {
+                packet[counter++] = (u_char) hostname.c_str()[j];
+            }
+
+            // System description
+            packet[counter++] = 0x0c; // System desc
+            packet[counter++] = (u_char) osname.length(); // Name length
+            for (int j = 0; j < osname.length(); ++j) {
+                packet[counter++] = (u_char) osname.c_str()[j];
+            }
+
+            // Caps
+            packet[counter++] = 0x0e; // Sys caps
+            packet[counter++] = 0x04; // size 2+2
+            packet[counter++] = 0x00; //
+            packet[counter++] = 0x80; // station only
+            packet[counter++] = 0x00; //
+            packet[counter++] = 0x80; // station only
+
+            // Management address
+            packet[counter++] = 0x10; // Management addr
+            packet[counter++] = 0x0c; // size 12
+            packet[counter++] = 0x05; // addr len 1+4
+            packet[counter++] = 0x01; // addr subtype: ipv4
+            packet[counter++] = (u_char) ip1; // ip
+            packet[counter++] = (u_char) ip2; // ip
+            packet[counter++] = (u_char) ip3; // ip
+            packet[counter++] = (u_char) ip4; // ip
+
+            packet[counter++] = 0x02; // if subtype: ifIndex
+            BYTE * pbyte = (BYTE *) &(pAdapterInfo->Index);
+            packet[counter++] = pbyte[3]; // id
+            packet[counter++] = pbyte[2]; // id
+            packet[counter++] = pbyte[1]; // id
+            packet[counter++] = pbyte[0]; // id
+
+            packet[counter++] = 0x00; // oid len 0
+
+            // IEEE 802.3 - MAC/PHY Configuration/Status
+            packet[counter++] = 0xfe; //
+            packet[counter++] = 0x09; //
+            packet[counter++] = 0x00; //
+            packet[counter++] = 0x12; //
+            packet[counter++] = 0x0f; //
+            packet[counter++] = 0x01; //
+            packet[counter++] = 0x02; //
+            packet[counter++] = 0x80; //
+            packet[counter++] = 0x00; //
+            packet[counter++] = 0x00; //
+            packet[counter++] = 0x1e; //
+
+            // IEEE 802.3 - Maximum Frame Size
+            packet[counter++] = 0xfe; //
+            packet[counter++] = 0x06; //
+            packet[counter++] = 0x00; //
+            packet[counter++] = 0x12; //
+            packet[counter++] = 0x0f; //
+            packet[counter++] = 0x04; //
+            packet[counter++] = 0x05; //
+            packet[counter++] = 0xee; //
+
+            // TIA TR-41 Committee - Media Capabilities
+            packet[counter++] = 0xfe; //
+            packet[counter++] = 0x07; //
+            packet[counter++] = 0x00; //
+            packet[counter++] = 0x12; //
+            packet[counter++] = 0xbb; //
+            packet[counter++] = 0x01; //
+            packet[counter++] = 0x01; //
+            packet[counter++] = 0xee; //
+            packet[counter++] = 0x03; //
+
+            // TIA TR-41 Committee - Network Policy
+            packet[counter++] = 0xfe; //
+            packet[counter++] = 0x08; //
+            packet[counter++] = 0x00; //
+            packet[counter++] = 0x12; //
+            packet[counter++] = 0xbb; //
+            packet[counter++] = 0x02; //
+            packet[counter++] = 0x06; //
+            packet[counter++] = 0x80; //
+            packet[counter++] = 0x00; //
+            packet[counter++] = 0x00; //
+
+            // TIA TR-41 Committee - Network Policy
+            packet[counter++] = 0xfe; //
+            packet[counter++] = 0x08; //
+            packet[counter++] = 0x00; //
+            packet[counter++] = 0x12; //
+            packet[counter++] = 0xbb; //
+            packet[counter++] = 0x02; //
+            packet[counter++] = 0x07; //
+            packet[counter++] = 0x80; //
+            packet[counter++] = 0x00; //
+            packet[counter++] = 0x00; //
+
+            // End of LLDPDU
+            packet[counter++] = 0x00; // type
+            packet[counter++] = 0x00; // len 0
 
             /* Send down the packet */
-            if (pcap_sendpacket(fp, packet, 100 /* size */) != 0) {
+            if (pcap_sendpacket(fp, packet, counter /* size */) != 0) {
                 fprintf(stderr, "\nError sending the packet: \n", pcap_geterr(fp));
             }
 
@@ -114,17 +232,4 @@ int main() {
     }
 
     return 0;
-}
-
-#define IPTOSBUFFERS    12
-
-char *iptos(u_long in) {
-    static char output[IPTOSBUFFERS][3 * 4 + 3 + 1];
-    static short which;
-    u_char *p;
-
-    p = (u_char *) &in;
-    which = (which + 1 == IPTOSBUFFERS ? 0 : which + 1);
-    sprintf(output[which], "%d.%d.%d.%d", p[0], p[1], p[2], p[3]);
-    return output[which];
 }
